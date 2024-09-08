c051bfa324bf57f22d46210c511b6d444f1300a7
Ranjeet Gautam <ranjitgautam893@gmail.com>
2024-09-08 10:40:08 +0545

    update

---
+++ javascript/fees_script.js
@@ -0,0 +1,190 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==1){
+        document.getElementById("demo").innerHTML ="Sanjay Chudali";
+        document.getElementById("demo1").innerHTML ="40000";
+    }
+    else if(inputVal==2)
+    {
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="Sandesh Bhandari";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="Sujan Bashyal";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==5)
+    {
+        document.getElementById("demo").innerHTML ="Hari chalise";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==6)
+    {
+        document.getElementById("demo").innerHTML ="Sagun Gautam";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="sagar poudel";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="prashiddha gyawali";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==9)
+    {
+        document.getElementById("demo").innerHTML ="shristi kc";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="samikshya karki";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="susma acharya";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==12)
+    {
+        document.getElementById("demo").innerHTML ="pooja paudel";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="Rajni Gautam";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="sudip Thapa";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="Aakash bhandari";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="Bishal pandey";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="sugam Gautam";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="sadhana Gautam";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="suvekshya Gautam";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="supriya Gautam";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==21)
+    {
+        document.getElementById("demo").innerHTML ="sudikshya Gautam";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==22)
+    {
+        document.getElementById("demo").innerHTML ="asmita panthi";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==23)
+    {
+        document.getElementById("demo").innerHTML ="Nishan Rana";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==24)
+    {
+        document.getElementById("demo").innerHTML ="susan baniya";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==25)
+    {
+        document.getElementById("demo").innerHTML ="Asmit Thapa";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==26)
+    {
+        document.getElementById("demo").innerHTML ="anuja chetri";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+     else if(inputVal==27)
+    {
+        document.getElementById("demo").innerHTML ="sunil gyawali";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==28)
+    {
+        document.getElementById("demo").innerHTML ="amrit chetri";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+
+    else if(inputVal==29)
+    {
+        document.getElementById("demo").innerHTML ="kshtiz acharya";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+     else if(inputVal==30)
+    {
+        document.getElementById("demo").innerHTML ="Ranjeet Gautam";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==31)
+    {
+        document.getElementById("demo").innerHTML ="Roshan bhandari";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+    else if(inputVal==32)
+    {
+        document.getElementById("demo").innerHTML ="Barsa Aryal";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==33)
+    {
+        document.getElementById("demo").innerHTML ="Namuna Bhandari";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==34)
+    {
+        document.getElementById("demo").innerHTML ="NIsha pandey";
+        document.getElementById("demo1").innerHTML ="20000 + 2500 = 22500"; 
+    }
+    else if(inputVal==35)
+    {
+        document.getElementById("demo").innerHTML ="Dibesha Gyawali";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
+
